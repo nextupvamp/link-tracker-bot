ac23ed0908e7bba7b7cd629f8c085ea8f729d346
nextupvamp <k.vlad.200404@gmail.com>
2025-02-08 11:31:01 +0300

    implemented chat repo

---
+++ scrapper/src/main/java/backend/academy/scrapper/model/Chat.java
@@ -0,0 +1,22 @@
+package backend.academy.scrapper.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class Chat {
+    private final long id;
+    private Set<Link> links = new HashSet<>();
+
+    public Optional<Link> findLink(String url) {
+        Link example = new Link(url);
+        return links.stream().filter(it -> it.equals(example)).findFirst();
+    }
+}
+
---
+++ scrapper/src/main/java/backend/academy/scrapper/repository/ChatRepository.java
@@ -0,0 +1,25 @@
+package backend.academy.scrapper.repository;
+
+import backend.academy.scrapper.model.Chat;
+import org.springframework.stereotype.Repository;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class ChatRepository {
+    private final Map<Long, Chat> chats = new HashMap<>();
+
+    public Optional<Chat> findById(long id) {
+        return Optional.ofNullable(chats.get(id));
+    }
+
+    public Chat save(Chat chat) {
+        chats.put(chat.id(), chat);
+        return chat;
+    }
+
+    public void delete(Chat chat) {
+        chats.remove(chat.id());
+    }
+}
