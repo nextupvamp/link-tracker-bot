757caaee98659c18ebc62319e8c39e2d965afedf
nextupvamp <k.vlad.200404@gmail.com>
2025-02-08 11:33:10 +0300

    added exception handling

---
+++ scrapper/src/main/java/backend/academy/scrapper/exception/ApiErrorResponse.java
@@ -0,0 +1,13 @@
+package backend.academy.scrapper.exception;
+
+import lombok.Builder;
+
+@Builder
+public record ApiErrorResponse(
+    String description,
+    int code,
+    String exceptionName,
+    String exceptionMessage,
+    StackTraceElement[] stackTrace
+) {
+}
