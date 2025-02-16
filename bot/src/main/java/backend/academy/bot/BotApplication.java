dcc464747f05b43612eaa3b3c4f95a8a42360225
nextupvamp <k.vlad.200404@gmail.com>
2025-02-16 17:33:29 +0300

    added bot commands registration on startup

--- bot/src/main/java/backend/academy/bot/BotApplication.java
+++ bot/src/main/java/backend/academy/bot/BotApplication.java
@@ -1,10 +1,18 @@
 package backend.academy.bot;
 
 import backend.academy.bot.client.ScrapperClient;
+import backend.academy.bot.service.Command;
 import backend.academy.bot.service.MessageHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.request.SendMessage;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -29,6 +37,7 @@ public class BotApplication {
     @Bean
     public TelegramBot bot(BotConfig botConfig, MessageHandler messageHandler) {
         TelegramBot bot = new TelegramBot(botConfig.telegramToken());
+        uploadCommands(botConfig.telegramToken());
         bot.setUpdatesListener(updates -> {
             updates.forEach(update -> {
                 if (update.message() != null) {
@@ -46,4 +55,33 @@ public class BotApplication {
         });
         return bot;
     }
+
+    private void uploadCommands(String token) {
+        List<BotCommandDto> commandDtos = new ArrayList<>();
+        Arrays.stream(Command.values()).forEach(command -> {
+            if (command.command() != null) {
+                commandDtos.add(new BotCommandDto(command.command(), command.description()));
+            }
+        });
+        SetCommandsRequest request = new SetCommandsRequest(commandDtos);
+        try {
+            URL url = new URL("https://api.telegram.org/bot" + token + "/setMyCommands");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(conn.getOutputStream(), request);
+
+        } catch (IOException e) {
+            LOG.atInfo().setMessage("Commands didn't upload successfully").log();
+            return;
+        }
+        LOG.atInfo().setMessage("Commands have been uploaded successfully").log();
+    }
+
+    private record BotCommandDto(String command, String description) {}
+
+    private record SetCommandsRequest(List<BotCommandDto> commands) {}
 }
