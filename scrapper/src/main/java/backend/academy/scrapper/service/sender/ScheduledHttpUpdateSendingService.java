e4281a93bd1d905badb14a1d73409805acd99946
nextupvamp <k.vlad.200404@gmail.com>
2025-03-22 18:45:24 +0300

    remade scheduled notifier

---
+++ scrapper/src/main/java/backend/academy/scrapper/service/sender/ScheduledHttpUpdateSendingService.java
@@ -0,0 +1,27 @@
+package backend.academy.scrapper.service.sender;
+
+import backend.academy.scrapper.ScrapperConfigProperties;
+import backend.academy.scrapper.client.BotHttpClient;
+import backend.academy.scrapper.service.scrapper.UpdateScrapperService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class ScheduledHttpUpdateSendingService implements UpdateSendingService {
+    private static final long RATE = 60_000;
+
+    private final BotHttpClient botHttpClient;
+    private final UpdateScrapperService scrapperService;
+
+    @Scheduled(fixedRate = RATE)
+    @Override
+    public void sendUpdate() {
+        log.info("Scheduled task is executing");
+        var updates = scrapperService.getUpdates();
+        updates.forEach(botHttpClient::sendUpdate);
+    }
+}
---
+++ scrapper/src/main/java/backend/academy/scrapper/service/sender/UpdateSendingService.java
@@ -0,0 +1,5 @@
+package backend.academy.scrapper.service.sender;
+
+public interface UpdateSendingService {
+    void sendUpdate();
+}
